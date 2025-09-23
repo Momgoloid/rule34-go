483fe1b69ee4005c62cc5447c98f89cb36f84d83
Momgoloid69 <momgoloid69@gmail.com>
2025-09-23 18:14:00 +0300

    updated usage example

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 
 	rule34 "github.com/Momgoloid/rule34-go/client"
 	"github.com/joho/godotenv"
@@ -12,37 +11,29 @@ import (
 
 func main() {
 	userID, apiKey := MustLoad()
-
 	rule34 := rule34.New(userID, apiKey)
 
-	post, err := rule34.GetPost(1)
+	post, err := rule34.Posts().Find()
 	if err != nil {
 		log.Fatalf("internal error: %v", err)
 	}
 
-	// _ = post
-
-	fmt.Println(post)
+	fmt.Println(post.Posts)
 }
 
-func MustLoad() (int, string) {
+func MustLoad() (string, string) {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Failed to load .env file: %v\n", err)
 	}
 
-	userIDStr := os.Getenv("USER_ID")
-	if userIDStr == "" {
+	userID := os.Getenv("USER_ID")
+	if userID == "" {
 		log.Fatal("Variable USER_ID is not set")
 	}
 	apiKey := os.Getenv("API_KEY")
 	if apiKey == "" {
 		log.Fatal("Variable API_KEY is not set")
-	}
-
-	userID, err := strconv.Atoi(userIDStr)
-	if err != nil {
-		log.Fatalf("Failed to convert string to int: %v", err)
 	}
 
 	return userID, apiKey
